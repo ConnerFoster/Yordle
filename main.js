2dbd4707af69a5e1248be1cffc6cdc24817de238
Conner Foster <connerf99@gmail.com>
2022-02-26 16:54:25 -0500

    Close to finish basic functionality in mainjs

--- main.js
+++ main.js
@@ -1,11 +1,51 @@
-let gamestate = []
+let word = "";
+let wordList = ["hello", "crane", "ghost", "check", "black", "docks"];
+let currentSquare = 0;
+let tiles = document.getElementsByClassName("tiles");
+let targetWord = getRandomWord();
+let row = 0;
 
-for (let i = 0; i < 25; i++) {
-    gamestate[i] = null
+function input(value) {
+  if (word.length < 5) {
+    word += value;
+    tiles[currentSquare].textContent = value;
+    currentSquare++;
+  }
+
+  console.log(word);
 }
 
-currentSquare = 0
+function del() {
+  word = word.slice(0, -1);
+  currentSquare--;
+  tiles[currentSquare].textContent = "";
+}
 
-function handleInput() {
-    
+function ent() {
+  if (word.length < 5) {
+    console.log("Too short a word");
+  } else if (!wordList.includes(word)) {
+    console.log("Not in word list");
+  } else {
+    wordCheck();
+  }
+}
+
+function wordCheck() {
+  if (word == targetWord) {
+    console.log("You Win");
+  } else if (row < 25) {
+    for (let i = 0; i < 5; i++) {
+      if (word[i] == targetWord[i]) {
+        tiles[row + i].classList.add("green");
+      } else if (targetWord.includes(word[i])) {
+        tiles[row + i].classList.add("yellow");
+      }
+    }
+    row += 5
+  }
+}
+
+function getRandomWord() {
+  return wordList[Math.floor(Math.random() * wordList.length)];
 }
