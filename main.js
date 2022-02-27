11842453599135b0faf2c6ee2eb8446ff36108fb
Conner Foster <connerf99@gmail.com>
2022-02-26 19:09:24 -0500

    making some aesthetic changes

--- main.js
+++ main.js
@@ -2,8 +2,17 @@ let word = "";
 let wordList = ["HELLO", "CRANE", "GHOST", "CHECK", "BLACK", "DOCKS"];
 let currentSquare = 0;
 let tiles = document.getElementsByClassName("tiles");
+let keys = document.getElementsByClassName("btn");
 let targetWord = getRandomWord();
 let row = 0;
+
+/* Still to do:
+add keyboard functionality
+add play again option
+dark mode
+make on-screen keyboard look better
+make on-screen keyboard keys color change too
+*/
 
 function input(value) {
   if (word.length < 5) {
@@ -23,9 +32,9 @@ function del() {
 
 function ent() {
   if (word.length < 5) {
-    console.log("Too short a word");
+    alert("Too short a word");
   } else if (!wordList.includes(word)) {
-    console.log("Not in word list");
+    alert("Not in word list");
   } else {
     wordCheck();
   }
@@ -37,6 +46,7 @@ function wordCheck() {
       tiles[row + i].classList.add("green");
     }
     alert("You Win!");
+    location.reload();
   } else if (row < 25) {
     for (let i = 0; i < 5; i++) {
       if (word[i] == targetWord[i]) {
