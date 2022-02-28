30c41abb86c06b7185467474695cbd3d1e109e5c
Conner Foster <connerf99@gmail.com>
2022-02-28 01:36:16 -0500

    Mostly working, need some visual fixes and bigger word list

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
+const fs = require("fs");
+
 let word = "";
 let wordList = ["HELLO", "CRANE", "GHOST", "CHECK", "BLACK", "DOCKS"];
 let currentSquare = 0;
-let tiles = document.getElementsByClassName("tiles");
-let keys = document.getElementsByClassName("btn");
+const tiles = document.getElementsByClassName("tiles");
+const keys = document.getElementsByClassName("btn");
+const modal = document.getElementById("modal-container");
 let targetWord = getRandomWord();
 let row = 0;
 let char;
-
 let currentRow = 0;
 
 /* Still to do:
-Make Modal for when game is over
-add play again option
+Make Modal look better
+make play again not just reload page, be able to keep track of wins - losses
 make on-screen keyboard look better
-make on-screen keyboard keys color change too
 add full word lists 
 keyboard right size on mobile
 */
@@ -52,16 +53,18 @@ function wordCheck() {
     for (let i = 0; i < 5; i++) {
       tiles[row + i].classList.add("green");
     }
-    alert("You Win!");
-    location.reload();
+    modal.style.display = "flex";
   } else {
     for (let i = 0; i < 5; i++) {
       if (word[i] == targetWord[i]) {
         tiles[row + i].classList.add("green");
+        updateKeyboardColors(word[i], "green");
       } else if (targetWord.includes(word[i])) {
         tiles[row + i].classList.add("yellow");
+        updateKeyboardColors(word[i], "yellow");
       } else {
         tiles[row + i].classList.add("gray");
+        updateKeyboardColors(word[i], "gray");
       }
     }
     row += 5;
@@ -70,6 +73,8 @@ function wordCheck() {
   }
   if (row == 30) {
     console.log("The Word was : " + targetWord);
+    document.getElementById("modal-main-text").textContent = "You Lost!🙁";
+    modal.style.display = "flex";
   }
 }
 
@@ -90,4 +95,28 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-function gameOver(result) {}
+function gameOver(result) {
+  return null;
+}
+
+function updateKeyboardColors(letter, color) {
+  for (let i = 0; i < 28; i++) {
+    if (keys[i].dataset.value == letter) {
+      keys[i].classList.add(color);
+    }
+    if (
+      keys[i].classList.contains("yellow") &&
+      keys[i].classList.contains("green")
+    ) {
+      keys[i].classList.remove("yellow");
+    }
+  }
+}
+
+document.getElementById("close-main-modal").addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+document.getElementById("play-again").addEventListener("click", () => {
+  location.reload();
+});
